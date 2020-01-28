0c0e66df56b6b063990bb96a7dd8606cc5e3212a
樋高菜未 <n.hidaka@weboar.com>
2020-01-28 19:02:58 +0900

    [update] DBからデータ取得の処理記述

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Stock;
+
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index() //追加
+    public function index()
     {
+        $stocks = Stock::Paginate(6); //Eloquantで検索
         return view('shop');
     }
 }
