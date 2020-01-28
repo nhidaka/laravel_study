6bf7a929308faf0b7c8efc781d3b39175bdf7145
樋高菜未 <n.hidaka@weboar.com>
2020-01-28 20:45:49 +0900

    [add] カートに入れる機能追加

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,6 +6,7 @@ use App\Models\Stock;
 use App\Models\Cart;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ShopController extends Controller
 {
@@ -19,5 +20,29 @@ class ShopController extends Controller
     {
         $carts = Cart::all(); //Eloquantで検索
         return view('mycart',compact('carts'));
+    }
+
+    public function addMycart(Request $request)
+    {
+        // ログイン状態のユーザーのidを取得
+        $user_id = Auth::id();
+
+        // POST送信で送られてきたstock_idを取得
+        $stock_id=$request->stock_id;
+        $message = '';
+
+        $cart_add_info=Cart::firstOrCreate(['stock_id' => $stock_id,'user_id' => $user_id]);
+
+        if($cart_add_info->wasRecentlyCreated){
+            $message = 'カートに追加しました';
+        }
+        else{
+            $message = 'カートに登録済みです';
+        }
+
+        $carts = Cart::where('user_id',$user_id)->get();
+
+        return view('mycart',compact('carts', 'message'));
+
     }
 }
--- resources/views/mycart.blade.php
+++ resources/views/mycart.blade.php
@@ -4,14 +4,21 @@
 <div class="container-fluid">
     <div class="">
         <div class="mx-auto" style="max-width:1200px">
-            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">カートの中身</h1>
+            <h1 class="text-center font-weight-bold" style="color:#555555;  font-size:1.2em; padding:24px 0px;">
+            {{ Auth::user()->name }}さんのカートの中身</h1>
+
             <div class="">
+                <p class="text-center">{{$message}}</p><br>
                 <div class="d-flex flex-row flex-wrap">
+
                     @foreach($carts as $cart)
-                        {{$cart->user_id}}<br>
-                        {{$cart->stock_id}}<br>
+                        <div class="mycart_box">
+                            <p>ユーザーID：{{$cart->user_id}}</p>
+                            <p>ストックID：{{$cart->stock_id}}</p>
+                        </div>
                     @endforeach
                 </div>
+                <a href="/">商品一覧へ</a>
             </div>
         </div>
     </div>
